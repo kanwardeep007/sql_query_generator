42d386a1cbff0e5a05f106114ca0641eae1d7302
tl-kanwar-baweja <kanwar.baweja@truelayer.com>
2023-08-09 02:34:12 +0100

    chore: WIP easy to now do the job

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
 use clap::Parser;
+use clap::Subcommand;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// List of ids separated by \n inside double quotes
-    ids: String,
+    /// String containing either path or list of ids
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    File {
+        /// lists test values
+        #[arg(short, long)]
+        path: String,
+    },
+    Ids {
+        /// lists test values
+        #[arg(short, long)]
+        ids: String,
+    },
 }
 
 fn main() {
     let cli = Cli::parse();
 
+    match &cli.command {
+        Commands::File { path } => {}
+        Commands::Ids { ids } => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+    }
     let lines = cli.ids;
+    let file_path = cli.file_path;
+    match lines {
+        Some(inner_lines) => {}
+        None => {}
+    }
     let mut sql_line: String = String::new();
     for line in lines.split("\n") {
         sql_line = sql_line + &format!("\'{}\',", line.to_string());
